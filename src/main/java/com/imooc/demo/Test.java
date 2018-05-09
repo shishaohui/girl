23375626150f1c027082b8c57600e629c3969477
shishaohui <shishaohui@we.com>
2018-05-09 14:09:19 +0800

    test commmit

---
+++ src/main/java/com/imooc/demo/Test.java
@@ -0,0 +1,11 @@
+package com.imooc.demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Integer a = 10;
+		Integer b = 20;
+		Integer c = a + b;
+		System.out.println("my test"+c);
+	}
+}
